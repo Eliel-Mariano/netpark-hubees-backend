e0b6c39d91a9959383f7b0a9358295da5e7a9331
Eliel Mariano <eliel.mariano@engenharia.ufjf.br>
2022-06-22 19:32:54 -0300

    correções pós feedback

--- src/business/DeleteBikeByIdBusiness.ts
+++ src/business/DeleteBikeByIdBusiness.ts
@@ -1,14 +1,15 @@
 import { DeleteBikeByIdDatabase } from "../data/DeleteBikeByIdDatabase";
 
-
 export class DeleteBikeByIdBusiness {
+  constructor(private deleteBikeByIdDatabase: DeleteBikeByIdDatabase) {}
 
-  constructor(
-    private deleteBikeByIdDatabase:DeleteBikeByIdDatabase
-  ){}
-    
-  deleteBike = async (id:string): Promise<void> => {
+  deleteBike = async (id: string): Promise<void> => {
+
     try {
+      const registeredBike = await this.deleteBikeByIdDatabase.findById(id);
+      if (registeredBike.length === 0) {
+        throw new Error("Bike não cadastrada.");
+      }
 
       await this.deleteBikeByIdDatabase.deleteBike(id);
 
